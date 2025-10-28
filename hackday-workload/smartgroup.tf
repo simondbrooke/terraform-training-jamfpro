1213373a7ccd55c553872408e872d8b5b4507130
bootrob <mcintyrerobert@outlook.com>
2025-10-28 14:23:40 +0000

    update smart group not checked in

--- hackday-workload/smartgroup.tf
+++ hackday-workload/smartgroup.tf
@@ -17,3 +17,24 @@ import {
   id = 226
   to = jamfpro_smart_computer_group.FileVault
 }
+
+
+  resource "jamfpro_smart_computer_group" "NotCheckedIn" {
+        name     = "Devices Not Checked In (30+ Days)"
+        site_id  = -1
+
+        criteria {
+          and_or        = "and"
+          closing_paren = false
+          name          = "Last Check-in"
+          opening_paren = false
+          priority      = 0
+          search_type   = "more than x days ago"
+          value         = "30"
+        }
+    }
+
+import {
+  id = 224
+  to = jamfpro_smart_computer_group.NotCheckedIn
+}
